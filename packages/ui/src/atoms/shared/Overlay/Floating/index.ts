62a8754574a0f9074a56307c36de83f9a8922a4d
Mbaruku2003 <leonardjembe21@gmail.com>
2026-07-21 13:50:13 +0300

    files in ContextMenu fixed

--- packages/ui/src/atoms/shared/Overlay/Floating/index.ts
+++ packages/ui/src/atoms/shared/Overlay/Floating/index.ts
@@ -1,31 +1,72 @@
 /**
  * ============================================================================
  * Ascend Enterprise UI
- * ContextMenu
+ * Floating
  * ----------------------------------------------------------------------------
- * Public exports
+ * File: index.ts
+ *
+ * Public exports for the Floating positioning engine.
+ *
  * ============================================================================
  */
 
-export { default as ContextMenu } from "./ContextMenuRoot";
-export { default as ContextMenuRoot } from "./ContextMenuRoot";
+/* -------------------------------------------------------------------------- */
+/* Runtime                                                                    */
+/* -------------------------------------------------------------------------- */
 
-export { default as ContextMenuTrigger } from "./ContextMenuTrigger";
-export { default as ContextMenuPortal } from "./ContextMenuPortal";
-export { default as ContextMenuContent } from "./ContextMenuContent";
+export {
+    useFloatingPosition,
+} from "./useFloatingPosition";
 
-export { default as ContextMenuItem } from "./ContextMenuItem";
-export { default as ContextMenuCheckboxItem } from "./ContextMenuCheckboxItem";
-export { default as ContextMenuRadioGroup } from "./ContextMenuRadioGroup";
-export { default as ContextMenuRadioItem } from "./ContextMenuRadioItem";
+/* -------------------------------------------------------------------------- */
+/* Utilities                                                                  */
+/* -------------------------------------------------------------------------- */
 
-export { default as ContextMenuGroup } from "./ContextMenuGroup";
-export { default as ContextMenuSeparator } from "./ContextMenuSeparator";
-export { default as ContextMenuLabel } from "./ContextMenuLabel";
-export { default as ContextMenuShortcut } from "./ContextMenuShortcut";
+export {
+    createVirtualElement,
+} from "./FloatingVirtualElement";
 
-export { default as ContextMenuSub } from "./ContextMenuSub";
-export { default as ContextMenuSubTrigger } from "./ContextMenuSubTrigger";
-export { default as ContextMenuSubContent } from "./ContextMenuSubContent";
+export {
+    getViewportRect,
+    isWithinViewport,
+} from "./FloatingViewport";
 
-export * from "./ContextMenu.types";
+/* -------------------------------------------------------------------------- */
+/* Types                                                                      */
+/* -------------------------------------------------------------------------- */
+
+export type {
+
+    FloatingPlacement,
+
+    FloatingPosition,
+
+    FloatingRect,
+
+    FloatingCoordinates,
+
+    FloatingBoundary,
+
+    FloatingMiddleware,
+
+    FloatingStrategy,
+
+    FloatingPositionOptions,
+
+    FloatingVirtualElement,
+
+} from "./FloatingPosition.types";
+
+/* -------------------------------------------------------------------------- */
+/* Constants                                                                  */
+/* -------------------------------------------------------------------------- */
+
+export {
+
+    DEFAULT_OFFSET,
+
+    DEFAULT_PADDING,
+
+    DEFAULT_STRATEGY,
+
+} from "./FloatingPosition.constants";
